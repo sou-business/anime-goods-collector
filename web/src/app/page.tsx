df27880c49ad4edb6f5a0c9cf66a1e2d24c30051
yamamoto <sou09business@gmail.com>
2025-08-18 11:48:49 +0900

    app_web作成

--- web/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/products/page.tsx
-import React from 'react';
-import ProductsPage from '@/ui/ProductsPage';
-
-const Page = () => {
-    return <ProductsPage />;
-};
-
-export default Page;
--- web/src/domain/repository/IProductRepository.ts
+++
@@ -1,7 +0,0 @@
-import { ProductModel } from "@/domain/model/ProductModel";
-
-export interface IProductRepository {
-  findAll(): Promise<ProductModel[]>;
-  createProducts(products: ProductModel[]): Promise<void>;
-  deleteAll(ids: number[]): Promise<void>;
-}
