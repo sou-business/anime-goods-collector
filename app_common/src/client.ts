7b203053fc5086a5bca5a48a4648541646dd02c0
sou <sou09business@gmail.com>
2026-01-27 12:18:51 +0900

    commonのclient.tsとserver.tsをモノレポ向けに修正

--- app_common/src/client.ts
+++ app_common/src/client.ts
@@ -1,3 +1,3 @@
-export * from '@/domain/model/ProductModel.js';
-export * from '@/utils/httpClient.js';
+export * from './domain/model/ProductModel.js';
+export * from './utils/httpClient.js';
 export type { Prisma, Product } from '@prisma/client';
--- app_common/src/server.ts
+++ app_common/src/server.ts
@@ -1,9 +1,9 @@
-export * from '@/domain/model/ProductModel.js';
-export * from '@/domain/repository/IProductRepository.js';
-export * from '@/domain/service/IProductScraper.js';
-export * from '@/utils/logger.js';
-export * from '@/utils/httpClient.js';
-export * from '@/infrastructure/cache/sharedCacheKeys.js';
-export * from '@/infrastructure/cache/memoryCacheService.js';
-export * from '@/infrastructure/repository/ProductsRepository.js';
+export * from './domain/model/ProductModel.js';
+export * from './domain/repository/IProductRepository.js';
+export * from './domain/service/IProductScraper.js';
+export * from './utils/logger.js';
+export * from './utils/httpClient.js';
+export * from './infrastructure/cache/sharedCacheKeys.js';
+export * from './infrastructure/cache/memoryCacheService.js';
+export * from './infrastructure/repository/ProductsRepository.js';
 export type { Prisma, Product } from '@prisma/client';
