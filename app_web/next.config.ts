e4bf473e5c92160c95335f2726fb72bfd5c765f0
sou <sou09business@gmail.com>
2026-03-08 16:16:48 +0900

    next.config.tsのwebpack設定をdev起動の時のみ適用

--- app_web/next.config.ts
+++ app_web/next.config.ts
@@ -1,26 +1,35 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/dist/shared/lib/constants";
 
 const domains = [
   '**.cospa.com',
   '**.amnibus.com',
 ];
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: domains.map((host) => ({
-      protocol: 'https',
-      hostname: host,
-    })),
-  },
-  webpack: (config: any, { dev }: any) => {
-    if (dev) {
+const nextConfig = (phase: string): NextConfig => {
+  
+  const config: NextConfig = {
+    images: {
+      remotePatterns: domains.map((host) => ({
+        protocol: 'https',
+        hostname: host,
+      })),
+    },
+  };
+  
+  // next dev (開発サーバー起動) の時だけ webpack 設定を適用
+  // turbopackはNext.jsのホットリロードに対応していないため、webpackでファイル変更を監視
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    config.webpack = (config) => {
       config.watchOptions = {
         poll: 1000,
         aggregateTimeout: 200,
       };
-    }
-    return config;
-  },
+      return config;
+    };
+  }
+
+  return config;
 };
 
 export default nextConfig;
